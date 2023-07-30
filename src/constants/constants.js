8d6d107c78044504a63511712009d2cf6e4b41db
J4M3$ <115919092+shibatj@users.noreply.github.com>
2023-07-30 20:43:10 +0700

    Add files via upload

---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+export const CONSTANTS  = {
+    'AUTHOR': 'Phanuwat Arsanok',
+    'AUTHOR_GITHUB_LINK': 'https://github.com/shibatj',
+}
---
+++ src/layout/Footer/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+import { chill, jazzy, sleep } from '../../data/songData';
+import { useSelector } from 'react-redux';
+import './styles.scss';
+import Player from '../../components/Player'
+import { CONSTANTS } from '../../constants/constants';
+
+const Footer = () => {
+  const data = useSelector((state) => state.mood);
+
+  const { moodMode } = data;
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+
+  return (
+    <div className='footer'>
+      <div className='song-name'>
+        {moodMode === 'chill' ? (
+          <span>Song name: {chill[currentSongIndex].name}</span>
+        ) : moodMode === 'jazzy' ? (
+          <span>Song name: {jazzy[currentSongIndex].name}</span>
+        ) : (
+          <span>Song name: {sleep[currentSongIndex].name}</span>
+        )}
+      </div>
+      <div className='controller'>
+        {moodMode === 'chill' ? (
+          <Player
+            currentSongIndex={currentSongIndex}
+            setCurrentSongIndex={setCurrentSongIndex}
+            songs={chill}
+          />
+        ) : moodMode === 'jazzy' ? (
+          <Player
+            currentSongIndex={currentSongIndex}
+            setCurrentSongIndex={setCurrentSongIndex}
+            songs={jazzy}
+          />
+        ) : (
+          <Player
+            currentSongIndex={currentSongIndex}
+            setCurrentSongIndex={setCurrentSongIndex}
+            songs={sleep}
+          />
+        )}
+      </div>
+      <div className='author'>
+        Made by:
+        <a
+          href={CONSTANTS.AUTHOR_GITHUB_LINK}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='author-name'
+        >
+         {CONSTANTS.AUTHOR}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
